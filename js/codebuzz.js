23e5cf77d484b704625d4df009acb12ea6517fc7
Andrew Walsh <andrew.jordan.walsh@gmail.com>
2021-02-11 14:32:54 -0600

    added codebuzz.js

---
+++ js/codebuzz.js
@@ -0,0 +1,16 @@
+//like the fizzbuzz problem, but with Codeup
+//divisible by 3, print CODE
+//divisible by 2, print UP
+//divisible by both, print CODEUP
+
+for (var i = 1; i < 100; i++) {
+    if (i % 6 === 0) {
+        console.log("CODEUP");
+    }
+    else if (i % 3 === 0) {
+        console.log("CODE");
+    }
+    else if (i % 2 === 0) {
+        console.log("UP");
+    }
+}
