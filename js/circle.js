4b26f25bd95337b5fecd0de1bda1e22030e58ff9
Andrew Walsh <andrew.jordan.walsh@gmail.com>
2021-03-01 16:24:41 -0600

    finished js math exercise

--- js/circle.js
+++ js/circle.js
@@ -13,11 +13,15 @@
 
         logInfo: function (doRounding) {
             // TODO: complete this method.
-
+            let result;
+            if (doRounding) {
+                result = Math.round(this.getArea());
+            }
+            else result = this.getArea();
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
 
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + result);
         }
     };
 
@@ -29,6 +33,7 @@
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
+    circle.radius = 5;
 
     // log info about the circle
     console.log("Raw circle information");
