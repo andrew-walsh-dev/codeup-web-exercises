56071f4e0bc5f69fd544c7851cd1afab4aac64fb
Andrew Walsh <andrew.jordan.walsh@gmail.com>
2021-04-09 18:32:38 -0500

    added marker and popup to mapbox

--- js/mapbox.js
+++ js/mapbox.js
@@ -1,14 +1,17 @@
-//map
-mapboxgl.accessToken = 'pk.eyJ1IjoiYW5kcmV3LXdhbHNoLWRldiIsImEiOiJja25hanBleHoxaTVjMndyaTFpN2VrcXBjIn0.tscRzZ-dBouiqJrT0DG6UA';
+mapboxgl.accessToken = MAP_BOX_APPID;
 var map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9 // starting zoom
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    zoom: 8,
+    center: [-98.4916, 29.4252]
 });
 
-
-
-geocode("Papa Johns", MAP_BOX_APPID).then(function(res){
-    console.log(res);
+geocode("Papa Johns San Antonio TX", MAP_BOX_APPID).then(function(res){
+    var papaJohnsMarker = new mapboxgl.Marker()
+    .setLngLat(res)
+    .addTo(map);
+    var papaJohnsPopup = new mapboxgl.Popup()
+    .setHTML("<p>Papa Johns!</p>")
+    
+    papaJohnsMarker.setPopup(papaJohnsPopup);
 })
