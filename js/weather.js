bdabec3eb30e27e3d6bfe6557e057916bc7fed66
Andrew Walsh <andrew.jordan.walsh@gmail.com>
2021-04-10 17:37:31 -0500

    setup api call to retrieve five day forecast

--- js/weather.js
+++ js/weather.js
@@ -1,11 +1,17 @@
-$.get("http://api.openweathermap.org/data/2.5/forecast", {
+$.get("https://api.openweathermap.org/data/2.5/forecast", {
     appid: OPEN_WEATHER_APPID,
     q: "San Antonio, US",
     units: "imperial",
-    count: 5
 })
 .done(function(data){
-    console.log(data);
+    var days = [];
+    for (var i = 0; i < data.list.length; i++){
+        if (i % 8 === 0) {
+            days.push(data.list[i]);
+        }
+    }
+    console.log(days);
+    // renderFiveDayForecast(days)
 })
 .fail(function(error){
     console.log(error);
@@ -13,3 +19,28 @@ $.get("http://api.openweathermap.org/data/2.5/forecast", {
 .always(function(){
     console.log("AJAX Request Finished.");
 })
+
+function getFiveDayForecast(location) {
+    $.get("https://api.openweathermap.org/data/2.5/forecast", {
+    appid: OPEN_WEATHER_APPID,
+    q: location,
+    units: "imperial",
+    cnt: 5
+    })
+    .done(renderFiveDayForecast(data.list))
+    .fail(function(error){
+        console.log(error);
+    })
+    .always(function(){
+        console.log("AJAX Request Finished.");
+    })
+}
+
+function renderFiveDayForecast(days) {
+    days.forEach(renderSingleForecast(day))
+}
+
+function renderSingleForecast(day){
+    
+    $("#forecast").html()
+}
