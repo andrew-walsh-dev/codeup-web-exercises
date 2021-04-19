54cd0d0977cbc2e4c2cbafc92818976985304b13
Andrew Walsh <andrew.jordan.walsh@gmail.com>
2021-04-19 12:08:01 -0500

    fixed github api request

--- js/promises.js
+++ js/promises.js
@@ -2,12 +2,10 @@ function getLatestPush(username) {
     fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': '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'}})
         .then(response => response.json())
         // .then(json => console.log(json))
-        .then(json => json.every((event) => {
-            if (event.type === "PushEvent"){
-                console.log("The latest push was on " + event.created_at.slice(0,10));
-                return false;
-            }
-        }))
+        .then(json => {
+            let lastPushDate = json.filter(event => event.type === "PushEvent")[0].created_at.slice(0,10);
+            console.log("Last push was on " + lastPushDate);
+        })
         .catch(error => console.error(error))
 }
 
